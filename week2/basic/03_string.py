fc1d1a5594f6232d40b00078951340c0fe48823f
rlawngud0428 <kjh000428@gmail.com>
2026-03-07 20:16:04 +0900

    basic 03_string 문제 해결

--- week2/basic/03_string.py
+++ week2/basic/03_string.py
@@ -36,8 +36,19 @@ def is_palindrome(s):
     """
     # TODO: 알파벳과 숫자만 남기고 소문자로 변환하세요
     # 힌트: isalnum() 메서드와 lower() 메서드 사용
-    pass
-    
+    test_string = ''.join(char.lower() for char in s if char.isalnum())
+
+    k = int(len(test_string) / 2)
+    revert = -1
+
+    for i in range(k):
+        if test_string[i] != test_string[revert]:
+            return False
+        else:
+            revert -= 1
+
+    return True
+
     # TODO: 정제된 문자열이 회문인지 확인하세요
     # 방법1: 문자열을 뒤집어서 비교 ([::-1] 사용)
     # 방법2: 양 끝 인덱스를 이용한 투 포인터 방식
