4aa91d4d0404c68070ec068078423b810d2296f9
rlawngud0428 <kjh000428@gmail.com>
2026-03-15 18:17:55 +0900

    난이도중 해시테이블로 세수의 합 문제 해결

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,24 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+
+N = int(input())
+
+num_list = set()
+for _ in range(N):
+    num_list.add(int(input()))
+
+sum_2_list = set()
+
+for i in num_list:
+    for j in num_list:
+        sum_2_list.add(i + j)
+
+result = 0
+
+for i in num_list:
+    for j in num_list:
+        if (i - j) in sum_2_list:
+            if (i > result):
+                result = i
+
+print(result)
