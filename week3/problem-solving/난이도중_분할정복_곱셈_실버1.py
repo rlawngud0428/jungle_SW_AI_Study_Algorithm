cfba32eabecf37135d0d2e2888d8b6989152afc0
rlawngud0428 <kjh000428@gmail.com>
2026-03-16 10:37:56 +0900

    난이도중 분할정복 곱셈 문제 해결

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,25 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+
+A, B, C = list(map(int, input().split()))
+
+result = 0
+
+def partition(B):
+    print(B)
+    if (B == 0):
+        return 1 % C
+    elif (B == 1):
+        print("B == 1")
+        return A % C
+    elif (B % 2 == 0):
+        print("B % 2 == 0")
+        half = partition(B//2)
+        return (half * half) % C
+    else:
+        print("B % 2 != 0")
+        half = partition(B//2)
+        return A * (half * half) % C
+    
+result = partition(B)
+print(result)
